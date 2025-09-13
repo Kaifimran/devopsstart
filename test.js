f1932d1907b9ed7e58c313fab07ca11c5d8c06b8
Kaifimran <102329904+Kaifimran@users.noreply.github.com>
2025-09-13 18:55:53 -0400

    start2againhopefully final

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 console.log("starting tests,,,,,");
-setTimeout(() =>  console.log{"waiting for 2 seconds"},2000);
+setTimeout(() =>  console.log(`{"waiting for 2 seconds"}`),2000);
 console.log("ending tests,,,,,");
